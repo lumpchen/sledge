7c108627ac544340343eddf5b104916af098f1ec
lumpchen <lumpchen@gmail.com>
2013-12-04 23:53:32 +0800

    To be prepared is half the victory.

--- src/me/lumpchen/sledge/pdf/graphics/GraphicsOperator.java
+++ src/me/lumpchen/sledge/pdf/graphics/GraphicsOperator.java
@@ -2,6 +2,8 @@ package me.lumpchen.sledge.pdf.graphics;
 
 import java.util.Stack;
 
+import me.lumpchen.sledge.pdf.syntax.basic.PReal;
+
 public abstract class GraphicsOperator {
 
 	public static enum Group {
@@ -16,7 +18,7 @@ public abstract class GraphicsOperator {
 	}
 	
 	public static final GraphicsOperator create(byte[] operatorBytes) {
-		return new OPre(operatorBytes);
+		return new OPre();
 	}
 	
 	public String toString() {
@@ -30,10 +32,76 @@ public abstract class GraphicsOperator {
 	abstract public void execute(Stack<GraphicsOperand> operandStack, VirtualGraphics g2d);
 }
 
+class OPq extends GraphicsOperator {
+	
+	public OPq() {
+		super(new byte[]{'q'});
+		this.operandNumber = 0;
+	}
+
+	@Override
+	public void execute(Stack<GraphicsOperand> operandStack, VirtualGraphics g2d) {
+		
+	}
+}
+
+
+class OPn extends GraphicsOperator {
+	
+	public OPn() {
+		super(new byte[]{'n'});
+		this.operandNumber = 0;
+	}
+
+	@Override
+	public void execute(Stack<GraphicsOperand> operandStack, VirtualGraphics g2d) {
+		
+	}
+}
+
+class OPcm extends GraphicsOperator {
+	
+	public OPcm() {
+		super(new byte[]{'c', 'm'});
+		this.operandNumber = 4;
+	}
+
+	@Override
+	public void execute(Stack<GraphicsOperand> operandStack, VirtualGraphics g2d) {
+		
+	}
+}
+
+class OPrg extends GraphicsOperator {
+	
+	public OPrg() {
+		super(new byte[]{'r', 'g'});
+		this.operandNumber = 3;
+	}
+
+	@Override
+	public void execute(Stack<GraphicsOperand> operandStack, VirtualGraphics g2d) {
+		
+	}
+}
+
+class OPW extends GraphicsOperator {
+	
+	public OPW() {
+		super(new byte[]{'W'});
+		this.operandNumber = 0;
+	}
+
+	@Override
+	public void execute(Stack<GraphicsOperand> operandStack, VirtualGraphics g2d) {
+		
+	}
+}
+
 class OPre extends GraphicsOperator {
 	
-	OPre(byte[] operatorBytes) {
-		super(operatorBytes);
+	public OPre() {
+		super(new byte[]{'r', 'e'});
 		this.operandNumber = 4;
 	}
 
@@ -44,7 +112,11 @@ class OPre extends GraphicsOperator {
 		GraphicsOperand operand_3 = operandStack.pop();
 		GraphicsOperand operand_4 = operandStack.pop();
 		
-		operand_1.asReal();
+		PReal x = operand_1.asReal();
+		PReal y = operand_2.asReal();
+		PReal w = operand_3.asReal();
+		PReal h = operand_4.asReal();
+		
 	}
 }
 
